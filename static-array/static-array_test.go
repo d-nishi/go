02521dca0b0058954e3f288183e75c1c4008da45
Nishi Davidson <nishidavidson@Nishis-MacBook-Pro.local>
2020-03-30 17:14:58 -0700

    add table driven test format

--- static-array/static-array_test.go
+++ static-array/static-array_test.go
@@ -1,16 +1,35 @@
 package main
 
 import (
-	"testing"
 	"reflect"
+	"testing"
 )
 
-func TestCreateArr(t *testing.T) {
-	got := CreateArr()
-	want:= []int{1,2,5,7,9}
-
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("got %q want %q", got,want)
-	}
+var tests = []struct {
+	name string
+	out  []int
+	want bool
+}{
+	{
+		name: "equals",
+		out:  []int{1, 2, 5, 7, 9},
+		want: true,
+	},
+	{
+		name: "not equals",
+		out:  []int{1, 1, 4, 7, 9},
+		want: false,
+	},
 }
 
+func TestCreateArr(t *testing.T) {
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := CreateArr()
+			equals := reflect.DeepEqual(got, tt.out)
+			if equals != tt.want {
+				t.Errorf("test failed")
+			}
+		})
+	}
+}
